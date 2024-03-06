a5295ea4cc9713f03d05b1442c25fd4600fcdafb
Igor Vinicius <161034058+IgorVads@users.noreply.github.com>
2024-03-06 12:47:01 -0300

    Update fatorial de impares.c
    correção no código

--- EX7.02/fatorial de impares.c
+++ EX7.02/fatorial de impares.c
@@ -1,28 +1,31 @@
 #include <stdio.h>
 #include <locale.h>
 
-// Um programa que calcula e exibe o fatorial dos n�meros �mpares existentes
+// Um programa que calcula e exibe o fatorial dos números ímpares existentes
 // no intervalo entre 1 (inclusive) e 11 (inclusive). 
 
 
 
 int main(void)
 {
-    int i;
+    int i,cont;
 	double fatorial = 1;
+	
 	setlocale(LC_ALL, "portuguese");
 	
-	
+	  printf("Fatorial dos números ímpares de 1 a 11:\n");
+	  
 		 for (i = 1; i <= 11; i++) 
-		 {
-			 if (!(i % 2 == 0));
-			 {
-				fatorial *= i; 
-				printf("%i! = %.0lf\n", i, fatorial);
-			 } 
-			
-         }
-
+		 { 
+		 if (i % 2 != 0) 
+		  {
+		 	for (cont = 1; cont <= i; cont++) {
+                fatorial *= cont;
+		   }
+			printf("%i! = %.0lf\n", i, fatorial); 
+			fatorial = 1;
+           }
+		 }
 	return 0;
 }
 		 
